9ad3de1193a0979aded09148f44e6ba2f59d306c
pereiralivia <pereira.livia@gmail.com>
2023-04-16 13:44:53 -0300

    fixed lint errors

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-const express = require("express");
-const app = express();
+const express = require('express')
+const app = express()
 
 // Heroku dynamically sets a port
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001
 
-app.use(express.static("dist"));
+app.use(express.static('dist'))
 
 app.listen(PORT, () => {
-  console.log("server started on port 3001");
-});
+  /* eslint-disable no-console */
+  console.log('server started on port 3001')
+})
